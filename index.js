4fcdbf576ceb2ed5b7cc53e789c519f93182c9de
Muahmmed Sahal <162565944+sahalnp@users.noreply.github.com>
2025-10-10 20:50:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 export const prisma = new PrismaClient();
 app.use(cors({
   origin: [
-    "https://kpi-trackingfrontent.vercel.app",
+    "https://kpi-tracking-front.vercel.app/",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
